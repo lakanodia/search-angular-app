317ef7d09cd3821d09fb7b0c9283ca6c42805aeb
Laka Nodia <laka.nodia@gmail.com>
2022-11-15 10:53:04 +0400

    Create search app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'search-angular-app';
+  selectedLocation: HousingLocation | undefined;
+
+  housingLocationList: HousingLocation[] = [
+    {
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '../assets/pic1.jpg',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '../assets/pic2.jpg',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '../assets/pic3.jpg',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+  ];
+
+  updateSelectedLocation(location: HousingLocation) {
+    this.selectedLocation = location;
+  }
 }
